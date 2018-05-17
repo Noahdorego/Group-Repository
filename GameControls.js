a414b09a256490362778cc0e5196611569123a2a
Noahdorego <36045890+Noahdorego@users.noreply.github.com>
2018-05-17 10:41:24 -0400

    Update GameControls.js

--- GameControls.js
+++ GameControls.js
@@ -2,15 +2,23 @@ function setup() {
     noStroke();
 }
 
+//Main Character
 var x = 200;
 var y = 380;
+
+//Score
 var score = 0;
-var randomX = random(0, 200);
-var randomX2 = random(201, 400);
+
+//Bird Spawning
+var xPositions = [random(0, 200), random(201, 400), random(0, 400)];
 var downY = 0;
 var downY2 = 0;
+var downY3 = 0;
+
+//Road & Car
 var roadY = 0;
 var carX = 0;
+
 var currentScene = 1;
 
 // Character
@@ -23,18 +31,27 @@ var player = fill(255, 0, 0);
 
 var drawBird = function(){
     fill(240, 218, 19);
-    rect(randomX + 5, downY + 10, 10, 15); 
+    rect(xPositions[0] + 5, downY + 10, 10, 15); 
     fill(255, 255, 255);
-    rect(randomX, downY, 20, 20); 
-    rect(randomX - 10, downY + 3, 40, 10);
+    rect(xPositions[0], downY, 20, 20); 
+    rect(xPositions[0] - 10, downY + 3, 40, 10);
 };
 
 var drawBird2 = function(){
     fill(240, 218, 19);
-    rect(randomX2 + 5, downY2 + 10, 10, 15); 
+    rect(xPositions[1] + 5, downY2 + 10, 10, 15); 
     fill(255, 255, 255);
-    rect(randomX2, downY2, 20, 20); 
-    rect(randomX2 - 10, downY2 + 3, 40, 10);
+    rect(xPositions[1], downY2, 20, 20); 
+    rect(xPositions[1] - 10, downY2 + 3, 40, 10);
+    
+};
+
+var drawBird3 = function(){
+    fill(240, 218, 19);
+    rect(xPositions[2] + 5, downY3 + 10, 10, 15); 
+    fill(255, 255, 255);
+    rect(xPositions[2], downY3, 20, 20); 
+    rect(xPositions[2] - 10, downY3 + 3, 40, 10);
     
 };
 
@@ -75,6 +92,7 @@ var drawScene1 =function(){
     text("Frogger", 115, 55);
 };
 
+//Losing Screen
 var drawScene3 = function() {
     background(84, 87, 176);
     
@@ -102,22 +120,25 @@ var drawScene2 =function(){
     fill(0, 0, 0);
     textSize(30);
     text(score, 0, 25);
-    
   
   drawCharacter();  
   
+    //Controls
+  
     if (keyIsPressed && keyCode === UP) {
-     y -= 4;   
+     y -= 2;   
     }
     if (keyIsPressed && keyCode === DOWN) {
-     y += 4;   
+     y += 2;   
     }
     if (keyIsPressed && keyCode === RIGHT) {
-     x += 4;   
+     x += 3;   
     }
     if (keyIsPressed && keyCode === LEFT) {
-     x -= 4;   
+     x -= 3;   
     }
+    
+    //Borders
     
     if (x < 10) {
      x = 10;   
@@ -142,13 +163,13 @@ var drawScene2 =function(){
     drawBird();
     if (downY > 410) {
         downY = 0;
-        randomX = random(0, 200);
+        xPositions[0] = random(0,200);
      }
     
-    downY += 2.5;
+    downY += 4;
     
     //Bird1 Collision
-    if (downY < y + 25 && downY > y - 25 && randomX < x + 10 && randomX > x - 30) {
+    if (downY < y + 25 && downY > y - 25 && xPositions[0] < x + 10 && xPositions[0] > x - 30) {
         drawScene3();
         currentScene = 3;
         downY = y;
@@ -159,21 +180,36 @@ var drawScene2 =function(){
     drawBird2();
     if (downY2 > 410) {
         downY2 = 0;
-        randomX2 = random(201, 350);
+        xPositions[1] = random(201, 400);
      }
      
-    
-    
-    downY2 += 3.5;
+    downY2 += 5;
     
     
     //Bird2 Collision
-    if (downY2 < y + 25 && downY2 > y - 25 && randomX2 < x + 10 && randomX2 > x - 30) {
+    if (downY2 < y + 25 && downY2 > y - 25 && xPositions[1] < x + 10 && xPositions[1] > x - 30) {
         drawScene3();
         currentScene = 3;
         downY2 = y;
     }
     
+    if (score > 1000) {
+     drawBird3();
+    if (downY3 > 410) {
+        downY3 = 0;
+        xPositions[2] = random(201, 350);
+     }
+    
+    downY3 += 3;   
+    }
+    
+    //Bird3 Collision
+    if (downY3 < y + 25 && downY3 > y - 25 && xPositions[2] < x + 10 && xPositions[2] > x - 30) {
+        drawScene3();
+        currentScene = 3;
+        downY3 = y;
+        downY2 = 500;
+    }
     
     if (roadY > 400) {
         roadY = 0;
@@ -193,7 +229,7 @@ var drawScene2 =function(){
     
 
 
-
+// Scene Drawing Loop
 draw = function() {
     if (currentScene === 2) {
         drawScene2();
@@ -201,20 +237,25 @@ draw = function() {
         drawScene1();
     } else if (currentScene === 3) {
         drawScene3();
+        xPositions += 1000;
     }
 };
 
 
+// Scene Change
 mouseClicked=function(){
     if (currentScene === 1) {
         drawScene2();
     } else if (currentScene === 3) {
+    xPositions = [random(0, 200), random(201, 400), random(0, 400)];
     downY = 0;
     downY2 = 0;
+    downY3 = 0;
     roadY = 0;
     carX = 0;
     x = 200;
     y = 380;
+    score = 0;
         drawScene2();
     }
 };
