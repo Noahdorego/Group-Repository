9662081738a4c2ef0cd349f4d9cdb7eb138f65b5
AlexWats <36045846+AlexWats@users.noreply.github.com>
2018-06-15 10:46:07 -0400

    Update LookBothWays.js

--- LookBothWays.js
+++ LookBothWays.js
@@ -1,7 +1,7 @@
 var song;
 function setup() {
 	song = loadSound('sound/media-d41f96a2.mp3');
-	song2 = loadSound('420356__eponn__crash.wav');
+	song2 = loadSound('sound/420356__eponn__crash.wav');
 	createCanvas(400, 400);
 	background(100);
 	noStroke();
